012790ccf60880abe744d8a36d1ff7fc8bd61bf7
aderonn <apbeuroonly@gmail.com>
2021-02-19 17:27:45 +0300

    project complete

---
+++ src/layout/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+    return <nav className="deep-purple lighten-2">
+    <div className="nav-wrapper">
+      <a href="#" className="brand-logo">Movies Finder</a>
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <li><a href="!#">My Repo</a></li>       
+      </ul>
+    </div>
+  </nav>
+}
+
+export {Header}
